eedf171b831704d8c8c8f7ef9f67ab5dca093985
Maggie9685 <maggielai9685@gmail.com>
2021-12-11 19:24:22 -0800

    add heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const notes = require('./db/db.json');
+const notes = require('./Develop/db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
 
 // Get Note
