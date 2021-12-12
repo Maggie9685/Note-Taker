5c476e3482d74366f9a3f92046f0bfc719e9b21a
Maggie9685 <maggielai9685@gmail.com>
2021-12-11 17:57:13 -0800

    add Delete feature

---
+++ Develop/server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Get Note
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(data);
+    }});
+});
+
+//Add Note
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNotes = {
+      title,
+      text,
+      id: Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+
+        parsedNotes.push(newNotes);
+
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 3),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
+      const newNotes = parsedNotes.filter((key) => key.id != req.params.id);
+
+      res.send(JSON.stringify(newNotes, null, 3));
+      
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(newNotes, null, 3),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+      );
+    }
+  });
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
